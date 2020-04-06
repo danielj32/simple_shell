716fd585649315db6daadc0fbad21f2d63236bb5
danielj32 <danieljuanito21@hotmail.com>
2020-04-06 21:57:29 +0000

    [create]- added new header file

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+#endif
