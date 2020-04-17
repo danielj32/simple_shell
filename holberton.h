c975e0372b4d25c87c77d69a37eb09ae09d45767
Juan Jose <1561@holbertonschool.com>
2020-04-17 03:57:36 +0000

    add prototypes

--- holberton.h
+++ holberton.h
@@ -8,28 +8,14 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
-#define DELIMS " \t\0"
+
 
 int _putchar(char c);
-void _printenv(void);
-extern char **environ;
 int _strlen(char *s);
-char *rdline(ssize_t *c);
-int env(char **env);
-char **parseString(char *lineArg, char *separator);
-void checkIfExit(char **parse, char *lineArg, int cmp);
-int _strLen(char *s);
 char *envir(const char *name, char **env);
 char *str_concat(char *s1, char *s2);
-int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 void print_matriz(char *av);
-int main(int ac, char **av, char **env);
-int exec(char **ar, char **env, char **argv, char *line, char *nline, int j);
-int C_d(char *line);
-char **str_split(char *line);
-char *m_alloc(char *c);
-void free_all(char *line, char *nline, char **tok);
-int ctrld(char *line);
+int main(void);
 
 #endif
